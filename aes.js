8d7599e334505617bf85a6264fb60a30e6130d61
karankumarslicepay <karan.kumar@sliceit.com>
2020-06-06 15:35:15 +0530

    start

---
+++ aes.js
@@ -0,0 +1,10 @@
+const encrypt = require('./encrypt');
+const decrypt = require('./decrypt');
+// pull the mode, file and password from the command arguments.
+const [ mode, file, password ] = process.argv.slice(2);
+if (mode === 'encrypt') {
+  encrypt({ file, password });
+}
+if (mode === 'decrypt') {
+  decrypt({ file, password });
+}
